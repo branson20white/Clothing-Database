202d35e39ec128ea5a6f93d87a8dd8040f93ee05
branson20white <bwhit128@students.kennesaw.edu>
2021-11-19 22:02:55 -0500

    Update database.py
    create sql table outline

--- database.py
+++ database.py
@@ -9,6 +9,17 @@ crsr = connection.cursor()
 # print statement will execute if there are no errors
 print("Connected to the database")
 
+# SQL command to create a table in the database
+sql_command = """CREATE TABLE clothes ( 
+item_name INTEGER PRIMARY KEY, 
+color VARCHAR(20), 
+fabric VARCHAR(20), 
+hood_or_not CHAR(3), 
+brand VARCHAR(20);"""
+  
+# execute the statement
+crsr.execute(sql_command
+
   
 # close the connection
 connection.close()
