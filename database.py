c2ac480f472f94908e7a27aedeaf769d882d7adf
branson20white <bwhit128@students.kennesaw.edu>
2021-11-20 07:38:37 -0500

    Update database.py
    populated sql table

--- database.py
+++ database.py
@@ -14,11 +14,39 @@ sql_command = """CREATE TABLE clothes (
 item_name INTEGER PRIMARY KEY, 
 color VARCHAR(20), 
 fabric VARCHAR(20), 
-hood_or_not CHAR(3), 
+hood CHAR(3), 
 brand VARCHAR(20);"""
-  
+
 # execute the statement
 crsr.execute(sql_command
+
+# primary key
+pk = [2, 3, 4, 5, 6]
+  
+# Enter 5 item names
+item_name = ['Atom', 'Ansur', 'Beta', 'Superior Down', 'Event']
+  
+# Enter 5 colors
+color = ['Blue', 'Brown', 'Maroon', 'Brown', 'Green']
+  
+# Enter 5 fabrics
+fabric = ['Down', 'Ripstop', 'Goretex', 'Down', 'Ripstop']
+  
+# Enter hood variable 
+hood = ['No', 'Yes', 'Yes', 'No', 'Yes']
+
+#enter name of brand 
+brand = ['Arcteryx', 'Klattermusen', 'Arcteryx', 'Mont Bell', 'And Wander']
+  
+for i in range(5):
+  
+    # This is the q-mark style:
+    crsr.execute(f'INSERT INTO emp VALUES ({pk[i]}, "{item_name[i]}", "{color[i]}", "{fabric[i]}", "{hood[i]}", "{brand[i]}")')
+  
+# To save the changes in the files. Never skip this.
+# If we skip this, nothing will be saved in the database.
+connection.commit()
+
 
   
 # close the connection
